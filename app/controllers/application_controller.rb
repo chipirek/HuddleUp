1c51d5faa37c356101a7fcc3ec89c1006e0cc182
Chip Irek <chip.irek@gmail.com>
2013-06-09 08:51:32 -0400

    Adding 'projects'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  before_filter :authenticate_user!
+  before_filter :make_sure_there_is_a_working_date
+
   protect_from_forgery
+
+  protected
+
+  def make_sure_there_is_a_working_date
+    session[:working_date] = Date.today.strftime("%m/%d/%Y") unless !session[:working_date].nil?
+  end
+
 end
