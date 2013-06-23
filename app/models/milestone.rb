bb8fbeab7c52cd329e6e1408bd5d01595b7d4018
Charles Irek <chip.irek@gmail.com>
2013-06-23 15:22:50 -0400

    Enabled dynamic sorting to tasks

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -7,4 +7,24 @@ class Milestone < ActiveRecord::Base
   belongs_to :project
   has_many :tasks
 
+  def total_points
+    p=0
+    tasks.each do |t|
+        p += t.points
+    end
+    return p
+  end
+
+  def calculated_percent_complete
+    tp = total_points + 1.00
+    cp = 0.00
+    tasks.each do |t|
+      if t.is_complete
+        cp += t.points
+      end
+    end
+
+    return (cp/tp)*100.00
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,5 @@ HuddleUp::Application.routes.draw do
   #match ':controller(/:action(/:id(.:format)))'
   match '/projects/:project_id/:controller/:action(/:id)'
   match '/projects/:project_id/milestones/:milestone_id/:controller/:action(/:id)'
+  match '/projects/:project_id/milestones/:milestone_id/:controller/:action' #sort
 end
