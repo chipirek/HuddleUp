069253cf534613a7e8aec1e062944793a4f2bd07
Chip Irek <chip.irek@gmail.com>
2013-07-18 06:48:11 -0400

    Add smoke test

--- test/integration/smoke_test.rb
+++ test/integration/smoke_test.rb
@@ -1,12 +1,11 @@
 require 'test_helper'
 
-class UserFlowsTest < ActionDispatch::IntegrationTest
+class SmokeTest < ActionDispatch::IntegrationTest
 
   fixtures :all
 
   test "login and get to index" do
-
-    @user = users(:me)
+    #@user = users(:me)
 
     get "/users/sign_in"
     assert_response :success
@@ -23,7 +22,6 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
 
 
   test "unauthenticated_access_blocked" do
-
     @user = users(:me)
 
     get "/projects"
@@ -31,7 +29,32 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
 
     request_via_redirect 'get', 'users/sign_in'
     assert_response :success
+  end
+
+
+  test "attempted sign in fails with bad credentials" do
+    get "/users/sign_in"
+    assert_response :success
+
+    post_via_redirect 'users/sign_in', 'user[email]' => 'chip.irek@gmail.com', 'user[password]' => 'wrong'
+    assert_equal "/users/sign_in", path
+    assert_equal 'Invalid email or password.', flash[:alert]
+  end
+
+
+  test "registration OK" do
 
   end
+
+
+  test "registration fails with missing (main) fields" do
+
+  end
+
+
+  test "registration fails with missing project name" do
+
+  end
+
 
 end
