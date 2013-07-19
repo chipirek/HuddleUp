2fa0f0ff141d626fef26f93c134d87a5085f6943
Charles Irek <chip.irek@gmail.com>
2013-07-19 13:34:29 -0400

    Completed adding smoke test

--- test/integration/smoke_test.rb
+++ test/integration/smoke_test.rb
@@ -36,24 +36,36 @@ class SmokeTest < ActionDispatch::IntegrationTest
     get "/users/sign_in"
     assert_response :success
 
-    post_via_redirect 'users/sign_in', 'user[email]' => 'chip.irek@gmail.com', 'user[password]' => 'wrong'
+    post_via_redirect '/users/sign_in', 'user[email]' => 'chip.irek@gmail.com', 'user[password]' => 'wrong'
     assert_equal "/users/sign_in", path
     assert_equal 'Invalid email or password.', flash[:alert]
   end
 
 
   test "registration OK" do
+    get "/users/sign_up"
+    assert_response :success
 
+    post_via_redirect '/users', 'user[name]' => 'New User','user[email]' => 'new_user@gmail.com', 'user[password]' => 'lollip0p', 'user[password_confirmation]' => 'lollip0p', 'project_name' => 'New Project'
+    assert_equal "/", path
   end
 
 
-  test "registration fails with missing (main) fields" do
+  test "registration fails with missing fields" do
+    get "/users/sign_up"
+    assert_response :success
 
+    post_via_redirect '/users', 'user[name]' => 'New User3', 'user[password]' => 'lollip0p', 'project_name' => 'New Project'
+    assert_equal "/users", path
   end
 
 
   test "registration fails with missing project name" do
+    get "/users/sign_up"
+    assert_response :success
 
+    post_via_redirect '/users', 'user[name]' => 'New User3','user[email]' => 'new_user3@gmail.com', 'user[password]' => 'lollip0p', 'user[password_confirmation]' => 'lollip0p'
+    assert_equal "/users", path
   end
 
 
