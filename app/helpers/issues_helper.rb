131dc0f9369c8b86c1d0ef8546ec07a29772313d
Chip Irek <chip.irek@gmail.com>
2013-11-14 14:24:55 -0700

    Fixing UI for issues and posts

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -1,2 +1,19 @@
 module IssuesHelper
+
+  def get_issue_status_color2(i)
+    if i.is_resolved?
+      return 'item-green'
+    else
+      return 'item-red'
+    end
+  end
+
+  def get_issue_status_color3(i)
+    if i.is_resolved?
+      return 'complete'
+    else
+      return 'incomplete'
+    end
+  end
+
 end
