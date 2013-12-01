84813705a604a58880890d499483a4451613d2f3
Charles Irek <chip.irek@gmail.com>
2013-12-01 10:40:44 -0500

    v0.9.2 - add CanCan authorization

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,14 +3,14 @@ class Ability
 
   def initialize(user)
 
+    can :manage, [Todo, Milestone, Issue, ActionItem, Invitation, Member, Post]
+
     membership = Member.where('user_id=' + user.id.to_s).where("status_code <> '9'").pluck(:project_id)
     projects = Project.where('id in (?)', membership)
 
     projects.each do |project|
       can :manage, Project, :id => project.id
     end
-
-    can :manage, [Todo, Milestone, Issue, ActionItem, Invitation, Member, Post]
 
   end
 
