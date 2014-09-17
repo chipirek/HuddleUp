77281235de3f2f15f368559ce9b069cecdc290de
Charles Irek <chip.irek@gmail.com>
2014-09-17 15:03:50 -0400

    Implementing new UX in issues module

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -1,19 +1,9 @@
 module IssuesHelper
 
-  def get_issue_status_color2(i)
-    if i.is_resolved?
-      return 'item-green'
-    else
-      return 'item-red'
-    end
-  end
-
-  def get_issue_status_color3(i)
-    if i.is_resolved?
-      return 'complete'
-    else
-      return 'incomplete'
-    end
+  def get_issue_last_updated_by(iid)
+    last_updated = Audit.where('(auditable_type=? and auditable_id=?) or (associated_type=? and associated_id=?)', 'Issue', iid, 'Issue', iid).order('created_at desc').first
+    who = User.find(last_updated.user_id).name
+    return who + ', ' + time_ago_in_words(last_updated.created_at) + ' ago'
   end
 
 end
