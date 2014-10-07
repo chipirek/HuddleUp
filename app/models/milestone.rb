6bca1ffae15cc21325ed882e1e84a90561fbbd48
Chip Irek <chip.irek@gmail.com>
2014-10-07 07:37:35 -0400

    Adding complex date and time validation to model

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -5,10 +5,41 @@ class Milestone < ActiveRecord::Base
   attr_accessible :project_id, :title, :start, :end, :all_day, :class_name, :icon,
                   :description, :start_time, :end_time
 
+  belongs_to :project
+
   validates_presence_of :title
   validates_presence_of :start
 
-  belongs_to :project
+  validate :date_logic_is_ok,
+           :time_logic_is_ok
+
+  def date_logic_is_ok
+    if self.end.nil?
+      all_day = true
+      return
+    end
+
+    if self.end < start
+      errors.add(:end, "can't be less than the start date.")
+    end
+  end
+
+  def time_logic_is_ok
+    if end_time.nil?
+      all_day = true
+      return
+    end
+
+    if start_time.nil?
+      all_day = true
+      return
+    end
+
+    if end_time < start_time
+      errors.add(:end_time, "can't be less than the start time.")
+    end
+
+  end
 
 
 end
