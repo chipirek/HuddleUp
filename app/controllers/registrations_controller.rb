b4a506f7dbe288c6b7cb3860a25870d502fe8fd0
Charles Irek <chip.irek@gmail.com>
2014-10-24 09:53:11 -0400

    Commercialization#119 - Refactored UX for devise actions so be away from billing info.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,13 @@
 class RegistrationsController < Devise::RegistrationsController
 
+
   layout "devise/new_registration", only: [:new]
+
 
   def new
     super
   end
+
 
   # this method overrides the base class
   def create
@@ -20,46 +23,12 @@ class RegistrationsController < Devise::RegistrationsController
       sign_up(resource_name, resource)
       respond_with resource, :location => after_sign_up_path_for(resource)
     end
-
-=begin
-    # --- build the devise user, called a 'resource'
-    build_resource
-
-    if resource.save   #--- if it saves ok...
-
-      #--- create a project object
-      project = Project.new(:name=>params[:project_name], :status_code=>1)
-      if project.save   #--- if it saves ok...
-
-        #--- create a member object
-        member = Member.new(:project_id=>@project.id, :user_id=>resource.id, :joined_date=>Time.now, :is_admin=>true)
-        if member.save
-          set_flash_message :notice, :signed_up if is_navigational_format?
-          sign_up(resource_name, resource)
-          respond_with resource, :location => after_sign_up_path_for(resource)
-        else
-          resource.errors.add('Member', ' creation had a fatal error.')
-          clean_up_passwords resource
-          respond_with resource
-        end
-
-      else
-        resource.errors.add('Project', ' name cannot be blank.')
-        clean_up_passwords resource
-        respond_with resource, :location => '/users/sign_up'
-      end
-    else
-      resource.errors.add('Registration', ' failed.  Does this email already exist in the system?')
-      clean_up_passwords resource
-      respond_with resource
-    end
-=end
-
   end
 
 
   def update
     super
   end
+
 
 end
